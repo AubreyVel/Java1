5a4aeb23b7c246b28a6d655016ea16ee4603c898
AubreyVel <agvelasco@amarillocollege.com>
2025-03-17 14:03:34 -0500

    code change

--- Main.java
+++ Main.java
@@ -8,19 +8,52 @@ public class Main {
         //System.out.println("hello world"); //print text to screen Console.WriteLine();
         //how to do console.readln();
 
-        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Enter username");
+       // Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+       // System.out.println("Enter username");
 
-        String userName = scanner.nextLine();  // Read user input
-        System.out.println("Username is: " + userName);  // Output user input
+       // String userName = scanner.nextLine();  // Read user input
+        //System.out.println("Username is: " + userName);  // Output user input
 
-        System.out.println("what year were you born in?");
+       // System.out.println("what year were you born in?");
 
-        int year = scanner.nextInt();
+       // int year = scanner.nextInt();
 
-        int age = 2025-year;
+       // int age = 2025-year;
 
-        System.out.println("You are "+age+"years old.");
+       // System.out.println("You are "+age+"years old.");
+
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter First number");
+        int firstNum = myObj.nextInt();
+
+        System.out.println("Enter second number");
+        int secondNum = myObj.nextInt();
+
+        System.out.println("Enter third number");
+        int thirdNum = myObj.nextInt();
+
+        //System.out.println("largest number: " + Math.max(Math.max(firstNum, secondNum), Math.max(secondNum, thirdNum)));
+
+        int largest = 0;
+
+        if (firstNum>=secondNum && firstNum> thirdNum)
+        {
+            largest = firstNum;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
